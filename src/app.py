7fd0139140c4d1b1c617fe53029360d50941502e
Alex <axherrerax@gmail.com>
2021-05-06 18:59:56 -0400

    Agregados

--- src/app.py
+++ src/app.py
@@ -2,19 +2,21 @@ from flask import Flask, json, jsonify, request, render_template
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from flask_cors import CORS
-from models import db, 
+from models import db, User
+
 #JWT
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False #no errores si incluyo o no un / en una ruta ó endpoints
-app.config['DEBUG'] = True
-app.config['ENV'] = 'development'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['DEBUG'] = True  #Muestra errores del servidor
+app.config['ENV'] = 'development' #Evitar cortar y levantar el servidor
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
-db.init_app(app)
-Migrate(app, db)
+
+db.init_app(app)  #Vincula app con bd
+Migrate(app, db)  #Vincula comandos
 CORS(app)
 manager = Manager(app)
 manager.add_command("db", MigrateCommand)
@@ -23,12 +25,45 @@ manager.add_command("db", MigrateCommand)
 
 @app.route('/')
 def main():
-    return render_template('index.html')
+    return jsonify({'msg':'Exito'})
+
+
+@app.route('/registro', methods=['POST'])
+def registrar():
+    id=request.json.get('id')
+    user_name=request.json.get('user_name')
+    first_name = request.json.get('first_name')
+    last_name = request.json.get('last_name')
+    email = request.json.get('email')
+    password = request.json.get('password')
+    phone = request.json.get('phone')
+
+    #Guarda datos en la tabla User
+    user = User()
+    user.id =id
+    user.user_name=user_name
+    user.first_name=first_name
+    user.last_name =last_name
+    user.email=email
+    user.password=password
+    user.phone=phone
+
+    user.save()
+
+    return jsonify({'Enviado': user.serialize()})
+
+
+@app.route('/registro')
+def getData():
+    user = User.query.all()
+    user = list(map(lambda user: user.serialize(),user))
+    return jsonify(user),200
 
 
 
 
-
+if __name__ == '__main__':
+    manager.run()
 
 
 
--- src/models.py
+++ src/models.py
@@ -1,3 +1,115 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, String, Integer, ForeignKey
+
+
 
 db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__='users'
+    id = db.Column(db.Integer,primary_key=True)
+    user_name = db.Column(db.String(120),nullable=False)
+    first_name = db.Column(db.String(120),nullable=False)
+    last_name = db.Column(db.String(120),nullable=False)
+    email = db.Column(db.String(120),nullable=False)
+    password = db.Column(db.String(120),nullable=False)
+    phone = db.Column(db.Integer,nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_name":self.user_name,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "email": self.email,
+            ## No se Serializa"password":self.password,
+            "phone":self.phone
+            
+        }
+    
+    def save(self):
+        db.session.add(self)  #Insert
+        db.session.commit()   #Guarda
+
+
+
+class Profile(db.Model):
+    __tablename__ ='profiles'
+    id = db.Column(db.Integer,primary_key=True)
+    city = db.Column(db.String(120),nullable=False)
+    phone = db.Column(db.Integer,nullable=False)
+    gender = db.Column(db.String(10),nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "city": self.city,
+            "phone": self.phone,
+            "gender": self.gender,
+            "user_id": self.user_id
+        }
+
+class Order(db.Model):
+    __tablename__ ='order'
+    id = db.Column(db.Integer,primary_key=True)
+    original_value = db.Column(db.Integer,nullable=False)
+    dollar_value = db.Column(db.Integer,nullable=False)
+    date = db.Column(db.String(120),nullable=False) #Cambiar a dato fecha
+    payment_method = db.Column(db.String(120),nullable=False) #Ver lista?
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id))
+    addressee_id= db.Column(db.Integer, db.ForeignKey('addressee.id'))
+    status_id = db.Column(db.Integer, db.ForeignKey('status.id'))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "original_value": self.original_value,
+            "dollar_value": self.dollar_value,
+            "date": self.date,
+            "payment_method": self.payment_method,
+            "user_id":self.user_id,
+            "addressee":self.addressee_id,
+            "status_id":self.status_id
+        }
+
+class Addressee(db.Model):
+    __tablename__ ='addressee'
+    id = db.Column(db.Integer,primary_key=True)
+    first_name = db.Column(db.String(120),nullable=False)
+    last_name = db.Column(db.String(120),nullable=False)
+    city = db.Column(db.String(120),nullable=False)
+    phone = db.Column(db.Integer,nullable=False)
+    gender = db.Column(db.String(10),nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "city": self.city,
+            "phone": self.phone,
+            "gender":self.gender
+        }
+    
+class Status(db.Model):
+    __tablename__ ='status'
+    id = db.Column(db.Integer,primary_key=True)
+    description = db.Column(db.String(120),nullable=False)  #Ver lista?
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "description": self.description
+        }
+
+class Administrator(db.Model):
+    __tablename__ ='administrator'
+    id = db.Column(db.Integer,primary_key=True)
+    notification = db.Column(db.String(120),nullable=False)  #Ver como realizarla
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "notification": self.notification
+        }
